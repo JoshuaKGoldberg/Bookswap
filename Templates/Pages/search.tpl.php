328795f7bec9b61dee6bc5b51e238076effed236
Josh Goldberg <josh@fullscreenmario.com>
2014-02-18 17:09:01 -0500

    Fixed width of search page input
    It now stretches to fill horizontally from the left to the <select>.

--- Templates/Pages/search.tpl.php
+++ Templates/Pages/search.tpl.php
@@ -1,16 +1,20 @@
 <section>
   <h1 class="standard_main standard_vert giant">Search</h1>
-  
-  <form class="standard_width" id="search_form" onsubmit="event.preventDefault(); searchFull();">
-    <input id="search_input" type="text" placeholder="fully search our database from here" />
-    <select id="search_change">
-      <?php
-        $options = ['Title', 'Author(s)', 'Description', 'Publisher', 'Year', 'ISBN'];
-        echo '<option>' . implode('</option><option>', $options) . '</option>';
-      ?>
-    </select>
-    <input id="search_submit" type="submit" value="Go!" />
-  </form>
+
+  <div id="search_form">
+    <form id="search_form_wrapper" class="standard_main" onsubmit="event.preventDefault(); searchFull();">
+      <div id="input_wrapper">
+        <input id="search_input" type="text" placeholder="fully search our database from here"/>
+      </div>
+      <select id="search_change">
+        <?php
+          $options = ['Title', 'Author(s)', 'Description', 'Publisher', 'Year', 'ISBN'];
+          echo '<option>' . implode('</option><option>', $options) . '</option>';
+        ?>
+      </select>
+    <button id="search_submit" type="submit">Go!</button>
+    </form>
+  </div>
   
   <div id="search_full_res_holder" class="standard_main standard_vert">
     <div id="search_full_results"></div>
