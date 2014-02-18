68f80bcbb51d19f281b9fdd01cb59bae8147c9e2
Josh Goldberg <josh@fullscreenmario.com>
2014-02-18 00:03:55 -0500

    Books only show entry edits if they're yours
    The Books/Medium template now uses UserLoggedIn() and $_TARGS['user_id']
    to check if it's the current user's entry, instead of blindly printing
    the buttons.

--- Templates/Books/Medium.tpl.php
+++ Templates/Books/Medium.tpl.php
@@ -9,8 +9,10 @@
   $year = $_TARGS['year'];
   $pages = $_TARGS['pages'];
   
-  if(!UserLoggedIn() || !isset($_TARGS['user_id'])) $price = false;
-  else $price = getPriceAmount($_TARGS['price']);
+  // Optional arguments
+  $price = isset($_TARGS['price']) ? getPriceAmount($_TARGS['price']) : false;
+  if(UserLoggedIn() && isset($_TARGS['user_id']))
+    $current_user = $_TARGS['user_id'] == $_SESSION['user_id'];
 ?>
 <div class="book book-medium" <?php if($price) echo 'price="' . $price . '"'; ?>>
   <?php
@@ -20,6 +22,7 @@
     <!-- Entry information (if it's given) -->
     <?php if($price): ?>
     <div class="entry book_entry price"><?php echo $price; ?></div>
+    <?php if($current_user): ?>
     <div class="entry book_entry changes">
       <?php
         $action = $_TARGS['action'];
@@ -30,6 +33,7 @@
       <div class="entry_changes entry_delete" onclick='<?php echo $func_del; ?>'></div>
       <div class="entry_changes entry_edit" onclick='<?php echo $func_edit; ?>'></div>
     </div>
+    <?php endif; ?>
     <?php endif; ?>
     
     <!-- Book information -->
