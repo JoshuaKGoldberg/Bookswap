3aa13fbf4d011e748b41c3acc2090d67d4aebc5a
Diogenesthecynic <josh@fullscreenmario.com>
2014-02-22 22:25:32 -0500

    Fix for search placeholder's violation by quotes
    Fixes https://github.com/Diogenesthecynic/Bookswap/issues/71

--- Templates/Header/Search.tpl.php
+++ Templates/Header/Search.tpl.php
@@ -1,7 +1,7 @@
 <div id="header_search" class="headblock">
   
   <form id="header_search_form" onkeydown="searchStart(event);" onsubmit="event.preventDefault(); searchStartFull();">
-    <input id="header_search_input" type="text" placeholder="search <?php echo getSiteName(); if(UserLoggedIn()) echo ', ' . $_SESSION['username']; ?>" />
+    <input id="header_search_input" type="text" placeholder="search <?php echo getSiteName(); if(UserLoggedIn()) echo ', ' . htmlentities($_SESSION['username']); ?>" />
   </form>
   
   <div id="header_search_results" class="hidden">
