4e7f9f33ec956f63df7129d318866597d800bc7b
Aaron Sedlacek <aaronsedlacek@heffner-wl-328.dynamic.rpi.edu>
2014-02-28 15:05:42 -0500

    re-corrected my last name

--- Templates/Footer.tpl.php
+++ Templates/Footer.tpl.php
@@ -12,6 +12,6 @@
     ?></div>
     A fun, friendly RCOS project.
     <br>
-    Albert Armea, Sebastian Basch, Javier Camino, Josh Goldberg, Harish Lall, Evan MacGregor, and Aaron Sedlack.
+    Albert Armea, Sebastian Basch, Javier Camino, Josh Goldberg, Harish Lall, Evan MacGregor, and Aaron Sedlacek.
   </div>
 </footer>
