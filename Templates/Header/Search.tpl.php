ba288927142021e342597ae00afa5af867af3e27
Sebastian <ziberrr8@gmail.com>
2014-03-19 18:47:24 -0400

    Issues 31 and 52 addressed
    not sure if issue 26 is still a problem. Having trouble reinstalling
    site, so not sure what my changes actually look like. Search.tpl.php and
    book.css altered accordingly.

--- Templates/Header/Search.tpl.php
+++ Templates/Header/Search.tpl.php
@@ -2,6 +2,7 @@
   
   <form id="header_search_form" onkeydown="searchStart(event);" onsubmit="event.preventDefault(); searchStartFull();">
     <input id="header_search_input" type="text" placeholder="search <?php echo getSiteName(); if(UserLoggedIn()) echo ', ' . htmlentities($_SESSION['username']); ?>" />
+    <input id="header_search_submit" type="submit" placeholder="Search!"/>
   </form>
   
   <div id="header_search_results" class="hidden">
