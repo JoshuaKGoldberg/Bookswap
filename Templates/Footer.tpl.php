393260b5723392a9d6bc087c1110d53c98ecfbd5
Josh Goldberg <diogenesthecynic@hotmail.com>
2014-03-29 07:24:08 -0400

    Updated footer
    Removed Sebastian, added options for RCOS project adjectives

--- Templates/Footer.tpl.php
+++ Templates/Footer.tpl.php
@@ -1,17 +1,25 @@
 <footer>
   <div class="standard_main">
-    <div><?php
-      if(UserLoggedIn())
-        $links = ['Account', 'Search', 'Import', 'Log Out'];
-      else
-        $links = ['Search'];
-      for($i = 0, $len = count($links); $i < $len; ++$i)
-        $links[$i] = getLinkHTML(strtolower(str_replace(' ', '', $links[$i])), $links[$i]);
-      $links[$i] = getLinkExternal('https://github.com/Diogenesthecynic/Bookswap/', 'Github');
-      echo implode('<span>&sdot;</span>', $links);
-    ?></div>
-    A fun, friendly RCOS project.
+    <div>
+      <?php
+        if(UserLoggedIn())
+          $links = ['Account', 'Search', 'Import', 'Log Out'];
+        else
+          $links = ['Search'];
+        for($i = 0, $len = count($links); $i < $len; ++$i)
+          $links[$i] = getLinkHTML(strtolower(str_replace(' ', '', $links[$i])), $links[$i]);
+        $links[$i] = getLinkExternal('https://github.com/Diogenesthecynic/Bookswap/', 'Github');
+        echo implode('<span>&sdot;</span>', $links);
+      ?>
+    </div>
+      <?php
+        $choices = array(
+          'n aspiring, active',
+          ' fun, friendly'
+        );
+        echo 'A ' . $choices[array_rand($choices)] . ' RCOS project.';
+      ?>
     <br>
-    Albert Armea, Sebastian Basch, Javier Camino, Josh Goldberg, Harish Lall, Evan MacGregor, and Aaron Sedlacek.
+    Albert Armea, Javier Camino, Josh Goldberg, Harish Lall, Evan MacGregor, and Aaron Sedlacek.
   </div>
 </footer>
