f92a0e57e0be4f0714e9565b8a3353118328adc2
Josh Goldberg <diogenesthecynic@hotmail.com>
2014-04-02 12:13:56 -0400

    Slightly prettier verification page
    The second sentence is smaller now.

--- Templates/Pages/verification.tpl.php
+++ Templates/Pages/verification.tpl.php
@@ -15,6 +15,8 @@
     </h1>
     <p class="standard_main standard_vert medium">
       <?php if($email_is_edu): ?>We've sent an email to <strong><?php echo $email; ?></strong>. Click the link there to verify it.<br><?php endif; ?>
+    </p>
+    <p class="standard_main standard_vert small">
       Verifying your email as yours and from your institute helps keep our site safe and your recommendations relevant.
     </p>
   </div>
