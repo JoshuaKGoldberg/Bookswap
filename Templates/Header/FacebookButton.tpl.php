8c366d6cdb6db712fb77ac61c0e1f807bf7ab617
Josh Goldberg <diogenesthecynic@hotmail.com>
2014-04-16 19:01:17 -0400

    Facebook button reacts to site user status
    If they're logged into BookSwap it becomes smaller accordingly

--- Templates/Header/FacebookButton.tpl.php
+++ Templates/Header/FacebookButton.tpl.php
@@ -1,1 +1,17 @@
+<?php
+    // Logged in users are offered a smaller button than anonymous
+    $logged = UserLoggedIn();
+    
+    // If also logged in with Facebook, ignore this - they're good
+    if(UserLoggedInFacebook()) {
+        return;
+    }
+?>
+<?php if($logged): ?>
+<div id="facebook_button" onclick="FB.login();">
+    <img src='https://fbstatic-a.akamaihd.net/rsrc.php/v2/yE/r/pO2d5bFWS8j.png'>
+    Log In
+</div>
+<?php else: ?>  
 <div id="facebook_button" onclick="FB.login();">Log In With Facebook</div>
+<?php endif; ?>
