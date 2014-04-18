6603b3ada4352419eba3c1658066855cb1c546f4
Josh Goldberg <diogenesthecynic@hotmail.com>
2014-04-18 14:49:37 -0400

    Search button
    A "Go!" is now visible in the search bar, as an input[type=submit]

--- Templates/Header/Search.tpl.php
+++ Templates/Header/Search.tpl.php
@@ -1,6 +1,8 @@
 <div id="header_search" class="headblock">
   
   <form id="header_search_form" onkeydown="searchStart(event);" onsubmit="event.preventDefault(); searchStartFull();">
+    <input id="header_search_submit" type="submit" value="Go!" />
+    
     <input id="header_search_input" type="text" placeholder="search <?php echo getSiteName(); if(UserLoggedIn()) echo ', ' . htmlentities($_SESSION['username']); ?>" />
   
     <div id="header_search_results" class="hidden">
