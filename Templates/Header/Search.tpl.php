a027fb2cbaf6146f4d6b8f2883d9419c5cbe855f
Josh Goldberg <diogenesthecynic@hotmail.com>
2014-04-18 12:18:52 -0400

    Fix for thin search results
    #header_search_results is a child of #header_search_form, and uses the
    same width as it (both now 100% of #header_search_form's 99% width)

--- Templates/Header/Search.tpl.php
+++ Templates/Header/Search.tpl.php
@@ -2,10 +2,10 @@
   
   <form id="header_search_form" onkeydown="searchStart(event);" onsubmit="event.preventDefault(); searchStartFull();">
     <input id="header_search_input" type="text" placeholder="search <?php echo getSiteName(); if(UserLoggedIn()) echo ', ' . htmlentities($_SESSION['username']); ?>" />
-  </form>
   
-  <div id="header_search_results" class="hidden">
-    <div id="header_search_results_contents"></div>
-  </div>
+    <div id="header_search_results" class="hidden">
+      <div id="header_search_results_contents"></div>
+    </div>
+  </form>
   
 </div>
