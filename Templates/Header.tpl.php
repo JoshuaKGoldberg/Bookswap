c874209ded03c99c9e8bef89d3425260f6d1163c
FTLspawn <macgregor.evan@gmail.com>
2014-04-25 16:07:23 -0400

    Trying again. Bleeerrrrrrggggggg

--- Templates/Header.tpl.php
+++
@@ -1,9 +0,0 @@
-<header>
-  <div id="header_main" class="standard_main standard_width">
-  
-    <?php TemplatePrint("Header/Badge", $tabs + 6); ?>
-    
-    <?php TemplatePrint("Header/Search", $tabs + 6); ?>
-    
-  </div>
-</header>
