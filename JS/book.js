7806df9d1d6aca3ce65e2e7ec3358e609f3e98f4
Josh Goldberg <diogenesthecynic@hotmail.com>
2014-05-02 01:20:46 -0400

    Facebook post on entry submit
    When an entry is submitted, if the user is logged into Facebook, it will
    ask to post the action to their wall before reloading the page.

--- JS/book.js
+++ JS/book.js
@@ -1,28 +1,48 @@
 // Gathers information about a submit-entry request, then submits it
 // Can't use automated sendRequestForm because IDs might be duplicate
 function entryAddSubmit(event, isbn) {
-  var form = event.target;
-  sendRequest("publicEntryAdd", {
-    isbn: isbn,
-    title: form.getElementsByClassName("entry_title")[0].innerText,
-    state: form.getElementsByClassName("entry_state")[0].value,
-    action: form.getElementsByClassName("entry_action")[0].value,
-    dollars: form.getElementsByClassName("num_dollars")[0].value,
-    cents: form.getElementsByClassName("num_cents")[0].value
-  }, function(result) { entryAddFinish(result, event.target); });
-  
-  // Mention it's working
-  $(form).find("input[type=submit]").val("thinking...");
-  console.log(window.d = form);
+    var form = event.target,
+        settings = {
+            isbn: isbn,
+            title: form.getElementsByClassName("entry_title")[0].innerText,
+            state: form.getElementsByClassName("entry_state")[0].value,
+            action: form.getElementsByClassName("entry_action")[0].value,
+            dollars: form.getElementsByClassName("num_dollars")[0].value,
+            cents: form.getElementsByClassName("num_cents")[0].value
+        };
+    sendRequest("publicEntryAdd", settings, function(result) {
+        entryAddFinish(result, event.target, settings);
+    });
+
+    // Mention it's working
+    $(form).find("input[type=submit]").val("thinking...");
 }
 
-function entryAddFinish(result, form) {
-  // Display the result in the HTML
-  var displayer = form.getElementsByClassName("entry_results");
-  displayer[0].innerHTML = result;
-  $(form).find("input[type=submit]").val("Go!");
-  
-  // If it's a success, reload the page
-  if(result == "Entry added successfully!")
-    window.location.reload();
+function entryAddFinish(result, form, settings) {
+    // Display the result in the HTML
+    var displayer = form.getElementsByClassName("entry_results");
+    displayer[0].innerHTML = result;
+    $(form).find("input[type=submit]").val("Go!");
+
+    // If it's a success, check for FB integration, then reload the page
+    if(result == "Entry added successfully!") {
+        FB.getLoginStatus(function(status) {
+            // If logged in, try to post to Facebook, *then* reload
+            if(status.status == "status") {
+                facebookPost("Hey I want to " 
+                        + settings.action + " a " 
+                        + settings.title + ". "
+                        + "Any takers?\n\n "
+                        + window.location.url,
+                    function() {
+                        window.location.reload();
+                    }
+                );
+            }
+            // Otherwise just reload immediately
+            else {
+                window.location.reload();
+            }
+        });
+    }
 }
