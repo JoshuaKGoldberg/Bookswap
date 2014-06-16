5a3ce3c7e381c56a30c1c078273aa000a4598532
Josh Goldberg <diogenesthecynic@hotmail.com>
2014-06-16 00:12:47 -0700

    Added templates for emails to /Templates/Emails
    Also removed test.tpl.php. Hehe.

---
+++ Templates/Emails/Welcome.tpl.php
@@ -0,0 +1,6 @@
+<h2>Congratulations, <?php echo $_TARGS['username']; ?>!</h2>
+<p>
+    Your account on <?php echo getSiteName(); ?> is now active.
+    Go on and swap some books!
+</p>
+<p><em>   -<?php echo getSiteName(); ?> team</em></p>
