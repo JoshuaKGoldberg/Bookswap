a88978d3a0df3070c34330c6d013ce320afe8f46
Josh Goldberg <diogenesthecynic@hotmail.com>
2014-07-07 00:59:37 -0700

    Fix login for standardized output

--- JS/login.js
+++ JS/login.js
@@ -1,27 +1,21 @@
-/* Logging in
-*/
-
 function loginSubmit() {
-  sendRequestForm("publicUserLogin", ["email", "password"], loginComplete, true);
-  $("#login_submit").val("Thinking...");
+    sendRequestForm("publicUserLogin", ["email", "password"], loginComplete, true);
+    $("#login_submit").val("Thinking...");
 }
 
-function loginComplete(result) {
-  console.log("result was", result);
-  // If the result is "Yes", it was successful
-  if(result == '"Yes"') {
-    var message = "You've successfully logged in! ";
-    message += "You should be redirected to ";
-    message += "<a href='/account.php'>your profile page</a>";
-    message += " shortly; if not, click that link.";
-    $("#login_form_inside").html("<aside>" + message + "</aside>");
-    window.location.reload();
-  }
-  // Otherwise the information was incorrect
-  else {
-    $("#login_form_inside input:not([type=submit])")
-      .css("background-color", "#fee")
-      .css("border", "1px solid #733");
-    $("#login_submit").val("Try again!");
-  }
+function loginComplete(resultRaw) {
+    if (result.status === "success") {
+        var message = "You've successfully logged in! ";
+        message += "You should be redirected to ";
+        message += "<a href='/account.php'>your profile page</a>";
+        message += " shortly; if not, click that link.";
+        $("#login_form_inside").html("<aside>" + message + "</aside>");
+        window.location.reload();
+    }
+    else {
+        $("#login_form_inside input:not([type=submit])")
+            .css("background-color", "#fee")
+            .css("border", "1px solid #733");
+        $("#login_submit").val("Try again!");
+    }
 }
